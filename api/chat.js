dcd6cc2cad75f2a055f4897b18b07aab4ef85641
rt004rw25-droid <rt004rw25@gmail.com>
2026-05-11 18:30:01 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -58,7 +58,7 @@ module.exports = async (req, res) => {
 
     // request ke Gemini
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key=" +
+      "https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=" +
       process.env.GEMINI_API_KEY,
       {
         method: "POST",
